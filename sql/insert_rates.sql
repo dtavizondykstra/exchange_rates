bd41fd47574c3b08ef836c3b2b1d7ae16bc284b8
Dalia Tavizon-Dykstra <83558848+dtavizondykstra@users.noreply.github.com>
2025-07-03 21:38:49 -0600

    refactor: added column names to insert command

--- sql/insert_rates.sql
+++ sql/insert_rates.sql
@@ -2,4 +2,14 @@
 LOAD DATA LOCAL INFILE '{csv_file_path}'
 INTO TABLE {table}
 FIELDS TERMINATED BY ','
-IGNORE 1 LINES;
+IGNORE 1 LINES
+(
+    base_code,
+    target_code,
+    rate,
+    time_last_update_utc,
+    time_next_update_utc,
+    time_next_update_unix,
+    time_last_update_unix
+);
+
